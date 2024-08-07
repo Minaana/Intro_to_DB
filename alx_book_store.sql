5f2cfa8522ffeee1f83506e1ccaa7565b2c3ff49
Minaana <emmanuelnanaopokumanu@outlook.com>
2024-08-06 22:09:50 -0400

    Created a database

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `alx_book_store` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `alx_book_store`;
 -- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
 -- Host: localhost    Database: alx_book_store
@@ -155,4 +157,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-08-06 22:06:24
+-- Dump completed on 2024-08-06 22:09:39
